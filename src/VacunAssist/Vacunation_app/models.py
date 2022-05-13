a9386b1b7980413b673190ed2795ca9296a40b5a
LautaroTorchia <torchia.lautaro2@gmail.com>
2022-05-13 16:42:50 -0300

    cambie una variable

--- src/VacunAssist/Vacunation_app/models.py
+++ src/VacunAssist/Vacunation_app/models.py
@@ -47,9 +47,9 @@ class CustomUserManager( BaseUserManager):#Cambiar para que se vea más como eld
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
+            raise ValueError(('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+            raise ValueError(('Superuser must have is_superuser=True.'))
         return self.create_user(dni,password, **extra_fields)
 
 

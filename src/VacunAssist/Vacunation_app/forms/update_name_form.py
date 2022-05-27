7e58a94bb937531206553beb3a74cee242f7db02
Jurten <joaquiniolmos@gmail.com>
2022-05-27 11:48:50 -0300

    cambios al disenio antes de la demo

--- src/VacunAssist/Vacunation_app/forms/update_name_form.py
+++ src/VacunAssist/Vacunation_app/forms/update_name_form.py
@@ -1,10 +1,12 @@
 from django import forms
 from ..models import Vacunatorio
 
+
 class NameUpdateForm(forms.Form):
-    nombre_actual=forms.ModelChoiceField(required=False, widget=forms.Select, queryset=Vacunatorio.objects.all())
-    nombre_nuevo= forms.CharField(max_length=100)
-    def print_clean_data(self):
-
-        print(self.cleaned_data+"-"*500)
-
+    nombre_actual = forms.ModelChoiceField(required=False,
+                                           widget=forms.Select,
+                                           queryset=Vacunatorio.objects.all())
+    nombre_nuevo = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={'oninvalid': "setCustomValidity('Completa este campo')"}))

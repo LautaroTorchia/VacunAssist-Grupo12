f2b7af9ffc088290c86f61bebc81a453c2ce8540
Ciuel <63562254+Ciuel@users.noreply.github.com>
2022-06-14 17:34:34 -0300

    agrego vacuna fiebre amarilla a el home

--- src/VacunAssist/Vacunation_app/views/homes/patient_home_view.py
+++ src/VacunAssist/Vacunation_app/views/homes/patient_home_view.py
@@ -4,6 +4,9 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from Vacunation_app.models import Paciente,Turno
+from datetime import date
+from dateutil.relativedelta import relativedelta
 Usuario=get_user_model()
 
 
@@ -12,10 +15,20 @@ class HomeView(LoginRequiredMixin,TemplateView):
     permission_required = ("Vacunation_app.Paciente", )
 
     def get(self, request, *args, **kwargs):
+        paciente= Paciente.objects.get(user=request.user)
+        turnos= Turno.objects.filter(paciente=paciente)
+        self.extra_context={"turnos": turnos, 
+        "puede_fiebre_amarilla": self.puede_fiebre_amarilla(paciente),
+        "turno_amarilla": self.turno_amarilla(turnos[0:10])}#Slice por eficiencia
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         pass
+
+    def puede_fiebre_amarilla(self,paciente):
+        return paciente.user.fecha_nac.date()+relativedelta(years=60) > date.today() and not paciente.tuvo_fiebre_amarilla
+    def turno_amarilla(self,turnos):
+        return list(filter(lambda turno : "amarilla" in turno.vacuna.nombre,turnos))[0]
 
 def logout_view(request):
     logout(request)

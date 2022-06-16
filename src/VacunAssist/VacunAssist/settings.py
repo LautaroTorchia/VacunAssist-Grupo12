c8d479060756054cd87d75407d9f7c1bffae1e1b
Ciuel <63562254+Ciuel@users.noreply.github.com>
2022-06-16 14:56:01 -0300

    Intento para subir a pythonanywhere

--- src/VacunAssist/VacunAssist/settings.py
+++ src/VacunAssist/VacunAssist/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from django.contrib import messages
-
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = "aaaasasaaaa"#os.environ.get('SECRET_DJANGO_KEY')
+SECRET_KEY = os.environ.get('SECRET_DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

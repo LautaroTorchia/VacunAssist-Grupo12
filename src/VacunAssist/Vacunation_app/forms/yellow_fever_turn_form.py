6d4c5b2341bd9ce4c1a857d78462af994737be45
Ciuel <63562254+Ciuel@users.noreply.github.com>
2022-06-23 22:51:26 -0300

    Agregado el limite de la hora

--- src/VacunAssist/Vacunation_app/forms/yellow_fever_turn_form.py
+++ src/VacunAssist/Vacunation_app/forms/yellow_fever_turn_form.py
@@ -1,11 +1,11 @@
 from django import forms
 from Vacunation_app.forms.p import PForm
 from datetime import date
-import datetime
+from datetime import time
 
 class assigningYellowFeverTurn(PForm,forms.Form):
     fecha_del_turno = forms.DateField(
         widget=forms.DateInput(attrs={'type': 'date',"min":date.today()}),
         label="Fecha del turno")
     hora_del_turno = forms.TimeField(
-        widget=forms.TimeInput(attrs={'type': 'time','min': datetime.datetime.now().replace(hour=8,minute=0) }))
+        widget=forms.TimeInput(attrs={'type': 'time','min': time(8,0,0) ,'max': time(16,0,0) }))

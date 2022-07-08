eb2fd8fff910f5cc76741fd2ba88d4162a260303
Jurten <joaquiniolmos@gmail.com>
2022-07-08 11:30:45 -0300

    se agrego la funcionalidad de reasignar turnos de covid

--- src/VacunAssist/Vacunation_app/views/administrator/administrator_lists.py
+++ src/VacunAssist/Vacunation_app/views/administrator/administrator_lists.py
@@ -1,9 +1,12 @@
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
 from Vacunation_app.custom_functions import AbstractAdminListView
-from Vacunation_app.models import Vacunador, Paciente, listaDeEsperaFiebreAmarilla
+from Vacunation_app.models import Vacunador, Paciente, listaDeEsperaFiebreAmarilla, listaDeEsperaCovid
 from django.views.generic.list import ListView
 from typing import Any
 from django.http import HttpResponse,HttpRequest
-
+from Vacunation_app.turn_assignment import get_new_turn
+from django.contrib import messages
 
 
 class VaccinatorsList(AbstractAdminListView):
@@ -26,3 +29,17 @@ class PatientsList(AbstractAdminListView):
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         self.queryset= Paciente.objects.all()
         return super().get(request, *args, **kwargs)
+
+class ReasingCovidList(AbstractAdminListView):
+    template_name: str="administrator/covid_list.html"
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        self.queryset= listaDeEsperaCovid.objects.all()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if "reasignar" in request.POST:
+            reasing_turn=listaDeEsperaCovid.objects.get(id=request.POST.get("reasignar"))
+            get_new_turn(reasing_turn)
+            messages.success(request,f"Turno reasignado {reasing_turn}")
+        return redirect(reverse_lazy("covid_wait_list"))

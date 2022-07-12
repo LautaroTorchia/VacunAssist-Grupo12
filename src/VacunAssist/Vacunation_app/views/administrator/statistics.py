143dfea20138ab9fb59689ca3224231f1a711923
Ciuel <63562254+Ciuel@users.noreply.github.com>
2022-07-12 16:23:52 -0300

    Agregué las estadísticas

--- src/VacunAssist/Vacunation_app/views/administrator/statistics.py
+++ src/VacunAssist/Vacunation_app/views/administrator/statistics.py
@@ -17,7 +17,7 @@ class StatsView(AdministratorPermissionsMixin,TemplateView):
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         #series = df.groupby(["vacuna__nombre"])["vacuna__nombre"].count()
 
-        self.extra_context={"chart":self.proporcion_vacunaciones_chart(),"chart2":self.pacientes_por_zona(),"chart3":self.pacientes_por_edad()}
+        self.extra_context={"chart":self.proporcion_vacunaciones_chart(),"chart2":self.pacientes_por_zona(),"chart3":self.pacientes_por_grupo_etario()}
         return render(request,self.template_name,self.extra_context)
         
     def proporcion_vacunaciones_chart(self):
@@ -31,42 +31,28 @@ class StatsView(AdministratorPermissionsMixin,TemplateView):
         df = pd.DataFrame(nombres,columns=["Cantidad"])
         series = df.groupby(["Cantidad"])["Cantidad"].count()
         df=pd.DataFrame(series)
-        fig=px.bar(df)
-        fig.update_layout(
-                title_x=0.5,
-                height=500, 
-                width=800, 
-                hovermode='x unified', 
-                xaxis_tickformat =',d',
-                ## this will change the formatting for BOTH subplots
-                yaxis=dict(tickformat ='d'),
-                # template=symbol_template, 
-                separators=".", 
-                title={
-                    'text':"Pacientes por zona",
-                    'x':0.5
-                }
-            )
+        fig=px.bar(df, title='Cantidad de pacientes por zona',
+        labels={
+                     "value": "",
+                     "index": f"Zona con más pacientes: {series.idxmax()}"
+                 })
+        fig.update_layout(yaxis={"dtick":1})
         return fig.to_html()
 
-    def pacientes_por_edad(self):
+    def pacientes_por_grupo_etario(self):
         fechas=Paciente.objects.values_list("user__fecha_nac",flat=True)
-        fechas=list(map(lambda f: calculate_age(f),fechas))
-
-        fig = go.Figure()
-
-        fig.add_trace(go.Indicator(
-            value = sum(fechas)/len(fechas),
-            gauge = {
-                'axis': {'visible': False}},
-            domain = {'row': 0, 'column': 0}))
-
-        fig.update_layout(
-            grid = {'rows': 2, 'columns': 2, 'pattern': "independent"},
-            template = {'data' : {'indicator': [{
-                'title': {'text': "Edad promedio"},
-                'mode' : "number+delta+gauge"}]
-                                }})
-
+        edades=list(map(lambda f: calculate_age(f),fechas))
+        grupos=pd.DataFrame(edades,columns=["Edad"])
+        bins= [0,18,60,1000]
+        labels = ['Menor de 18','Entre 18 y 60','60 o más']
+        grupos['GrupoEtario'] = pd.cut(grupos['Edad'], bins=bins, labels=labels, right=False)
+        grupos=grupos.groupby("GrupoEtario").count()
+        fig = go.Figure(go.Indicator(
+            mode = "number+delta",
+            value = sum(edades)/len(edades),
+            title = {"text": "Edad promedio de los pacientes"},
+            domain = {'y': [0, 1], 'x': [0.25, 0.75]}))
+        fig.add_trace(go.Bar(
+            y=grupos["Edad"],x=grupos.unstack()["Edad"].index))
+        fig.update_layout(yaxis={"dtick":1},title="Cantidad de pacientes por grupo etario")
         return fig.to_html()
-

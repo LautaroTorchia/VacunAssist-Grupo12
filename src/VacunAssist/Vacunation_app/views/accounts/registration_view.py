f23849e169c00baf58a3857b06df7d3c35aa7094
Ciuel <63562254+Ciuel@users.noreply.github.com>
2022-07-14 16:40:50 -0300

    Solucione bug2

--- src/VacunAssist/Vacunation_app/views/accounts/registration_view.py
+++ src/VacunAssist/Vacunation_app/views/accounts/registration_view.py
@@ -45,6 +45,7 @@ def registration_view(request):
             assigner=TurnAssigner.get_assigner(patient)
             for vacu in NonRegisteredVacunacion.objects.filter(dni=form.cleaned_data["dni"]):
                 Vacunacion.crear_de_no_registrado(vacu,patient)
+                vacu.delete()
             assigner.assign_turns()
             vacunassist_send_mail('emails/registro_paciente.html',{"clave":clave, "dni":form.cleaned_data["dni"]}
             ,"Registro de vacunador a VacunAssist",form.cleaned_data.get("email"))
